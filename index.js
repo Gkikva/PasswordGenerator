71e5245b4ea592934173347b1f7898bde772435c
Gkikva <78609015+Gkikva@users.noreply.github.com>
2022-11-18 16:03:29 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+
+var ll = "1234567890-=qwertyuiop[]asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?"
+
+
+function blink_text() {
+	$('.PasswordText').fadeOut(500);
+    $('.PasswordText').fadeIn(500);
+}
+
+
+// when 20 symbols button is clicked
+document.querySelector(".TwentySymbolGenerator").addEventListener("click", function  () {
+    var random_text = ""
+    for (var i = 0; i< 20; i++) {
+        var random_symbols = Math.floor(Math.random() * ll.length);
+        random_text += ll[random_symbols]
+    };
+    console.log(random_text.length)
+    document.querySelector(".PasswordText").innerHTML= random_text;
+    blink_text();
+    setInterval(blink_text, 1000);
+});
+
+
+//  when 1-50 button is clicked
+
+document.querySelector("#passwordInputId").addEventListener("input", function () {
+    var input_value = this.value
+    if (input_value > 50) {
+        document.querySelector(".ManualGenerator").addEventListener("click", function () {
+            document.querySelector(".PasswordText").innerHTML= "You'r input is greater than 50"
+        })
+    }
+    else {
+        document.querySelector(".ManualGenerator").addEventListener("click", function  () {
+            var random_text = ""
+            for (var i = 0; i< input_value; i++) {
+                var random_symbols = Math.floor(Math.random() * ll.length);
+                random_text += ll[random_symbols]
+            };
+            console.log(random_text.length)
+            document.querySelector(".PasswordText").innerHTML= random_text;
+            blink_text();
+            setInterval(blink_text, 1000);
+        });
+    };
+    
+});
